3336e26bc17828d1f4131611466c58c434afb044
alema <alema@10.0.0.114>
2023-04-05 22:56:40 -0400

    Initial Commit

---
+++ src/demo/Test.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Test  {
+	
+	public static void main(String[] args) {
+		System.out.println("Test ........");
+		System.out.println("Batch 9 Git Lecture..");
+	}
+}
+
+
